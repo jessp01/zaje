121cd84376f8817844bece7eb120c461da727d54
Jesse Portnoy <jesse@packman.io>
2023-07-19 23:29:55 +0100

    gofmt

--- zaje.go
+++ zaje.go
@@ -37,9 +37,9 @@ func getDefs(filename string, data []byte) []highlight.LineMatch {
 		if syn_dir == "" {
 			if stat, err := os.Stat(os.Getenv("HOME") + "/.config/zaje/syntax_files"); err == nil && stat.IsDir() {
 				syn_dir = os.Getenv("HOME") + "/.config/zaje/syntax_files"
-			}else{
+			} else {
 				if stat, err := os.Stat("/etc/zaje/syntax_files"); err == nil && stat.IsDir() {
-				    syn_dir = "/etc/zaje/syntax_files"
+					syn_dir = "/etc/zaje/syntax_files"
 				}
 			}
 		}
